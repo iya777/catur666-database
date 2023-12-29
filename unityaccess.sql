12a27782bc7c7360e8ced29eff8f0c9e206d7f6d
Ilham <122004325+iya777@users.noreply.github.com>
2023-12-29 18:04:00 +0700

    Add files via upload

--- unityaccess.sql
+++ unityaccess.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 02, 2023 at 11:22 AM
+-- Generation Time: Dec 29, 2023 at 10:59 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `unityaccess`
 --
+CREATE DATABASE IF NOT EXISTS `unityaccess` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `unityaccess`;
 
 -- --------------------------------------------------------
 
@@ -31,8 +33,8 @@ CREATE TABLE `gamehistory` (
   `gameID` int(10) NOT NULL,
   `userID` int(10) NOT NULL,
   `gameEndedTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gameResult` varchar(10) NOT NULL,
-  `gameMode` varchar(10) NOT NULL,
+  `gameResult` varchar(255) NOT NULL,
+  `gameMode` varchar(255) NOT NULL,
   `vsAI` tinyint(1) NOT NULL,
   `difficultyAI` int(10) NOT NULL,
   `scoreGained` int(10) NOT NULL,
@@ -45,29 +47,13 @@ CREATE TABLE `gamehistory` (
 --
 
 INSERT INTO `gamehistory` (`gameID`, `userID`, `gameEndedTime`, `gameResult`, `gameMode`, `vsAI`, `difficultyAI`, `scoreGained`, `FEN`, `isWhite`) VALUES
-(1, 1, '2023-10-29 22:09:26', '1', 'BULLET', 1, 0, -4, 'r1b1k2r/pppp1ppp/4p3/2b1K1q1/4P3/8/PP4PP/nN5n w kq - 4 16', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `preference`
---
-
-CREATE TABLE `preference` (
-  `preferenceID` int(10) NOT NULL,
-  `userID` int(10) NOT NULL,
-  `fullscreen` tinyint(1) NOT NULL DEFAULT '1',
-  `masterVolume` float NOT NULL DEFAULT '100',
-  `musicVolume` float NOT NULL DEFAULT '100',
-  `sfxVolume` float NOT NULL DEFAULT '100'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `preference`
---
-
-INSERT INTO `preference` (`preferenceID`, `userID`, `fullscreen`, `masterVolume`, `musicVolume`, `sfxVolume`) VALUES
-(1, 1, 1, 100, 100, 100);
+(7, 5, '2023-11-02 20:15:11', '1', 'BULLET', 1, 0, -3, 'rn2kb1r/ppp2ppp/5p1n/3p3K/4Pq2/5b2/PPPP3P/RNB2B2 w kq - 6 11', 1),
+(8, 5, '2023-11-02 20:15:49', '1', 'BULLET', 1, 0, -7, 'r2qkb1r/ppp2ppp/8/3ppb2/2Pn4/3K4/PP1P2PP/RNBn1BNR w kq - 1 10', 1),
+(9, 5, '2023-11-02 20:17:22', '1', 'BULLET', 1, 0, -8, 'r1b1kb1r/pppp1ppp/1q1np3/2K5/8/8/P1P3PP/RN4Nn w kq - 2 15', 1),
+(10, 5, '2023-11-02 20:18:50', '1', 'BULLET', 1, 0, -8, 'r1b1kb1r/ppp2ppp/8/3pp1q1/3n4/6K1/PPPP2PP/RNBn1BNR w kq - 4 10', 1),
+(15, 5, '2023-11-23 23:27:27', '2', 'BULLET', 1, 0, -6, 'rnbq1b2/1p2pp1p/p1p5/k2p4/1P1Q4/2N2N2/P1PP1PPP/R1BQKB1R b KQ - 0 7', 0),
+(16, 5, '2023-12-17 20:42:40', '1', 'BULLET', 1, 0, -5, 'r3kb1r/ppp1pppp/2n5/3n1b2/8/4K3/PPP2PPP/R1Bq1B1R w kq - 2 5', 1),
+(17, 5, '2023-12-17 20:42:55', '1', 'BULLET', 1, 0, -7, 'rn2k1nr/ppp1bppp/3qp3/6K1/4p3/8/PPPP1P1P/RNBb1B1R w kq - 2 8', 1);
 
 -- --------------------------------------------------------
 
@@ -79,19 +65,21 @@ CREATE TABLE `savedgames` (
   `savedGameID` int(10) NOT NULL,
   `userID` int(10) NOT NULL,
   `FEN` varchar(255) NOT NULL,
-  `gameMode` varchar(10) NOT NULL,
+  `gameMode` varchar(255) NOT NULL,
   `vsAI` tinyint(1) NOT NULL,
   `difficultyAI` int(10) NOT NULL,
   `isWhite` tinyint(1) NOT NULL,
-  `lastModified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `lastModified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `white_time` int(11) NOT NULL DEFAULT '-1',
+  `black_time` int(11) NOT NULL DEFAULT '-1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `savedgames`
 --
 
-INSERT INTO `savedgames` (`savedGameID`, `userID`, `FEN`, `gameMode`, `vsAI`, `difficultyAI`, `isWhite`, `lastModified`) VALUES
-(1, 1, 'r1bqkbnr/pppppppp/8/8/8/4n1P1/PPP1PP1P/RN1QKBNR w KQkq - 0 5', 'BULLET', 1, 0, 1, '2023-10-30 23:20:32');
+INSERT INTO `savedgames` (`savedGameID`, `userID`, `FEN`, `gameMode`, `vsAI`, `difficultyAI`, `isWhite`, `lastModified`, `white_time`, `black_time`) VALUES
+(27, 5, 'r1bqkbnr/2p2ppp/p1p5/4p3/4P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq - 0 1', 'BLITZ', 1, 3, 0, '2023-12-29 17:17:24', 55, 79);
 
 -- --------------------------------------------------------
 
@@ -116,7 +104,15 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userID`, `username`, `hash`, `salt`, `score`, `win`, `lose`, `draw`, `highestScore`) VALUES
-(1, 'player', '$5$rounds=5000$gabrielsukapokem$4Lm.oBDwjk6ERHx3.yPhnfW3nXQmGCfficFBhKgZqK7', '$5$rounds=5000$gabrielsukapokemonplayer$', 976, 0, 0, 0, 1000);
+(5, 'gabriel', '$5$rounds=5000$gabrielsukapokem$kMQmLOvFZP2alpq2qgNk.Xdqh77lImJKZrFHPyONi00', '$5$rounds=5000$gabrielsukapokemongabriel$', 950, 0, 2, 0, 1000),
+(6, 'data2', '$5$rounds=5000$gabrielsukapokem$gxuDw5tgn75ujyYwcq7W1a5p4rLkUktHEVRut/tGKj4', '$5$rounds=5000$gabrielsukapokemonwqrwer$', 1000, 0, 0, 0, 1000),
+(7, 'data3', '$5$rounds=5000$gabrielsukapokem$Vu8ztiEKv9ETp9yHem1m5XHE5Zx8jaxcetA5R6utg12', '$5$rounds=5000$gabrielsukapokemoncxvsfdf$', 1000, 0, 0, 0, 1000),
+(8, 'data4', '$5$rounds=5000$gabrielsukapokem$sUa4VVZO8lXZlWMdAQp9HB/MOlgT/re0bdrCP2p5jUC', '$5$rounds=5000$gabrielsukapokemonvcxvd$', 1000, 0, 0, 0, 1000),
+(9, 'data5', '$5$rounds=5000$gabrielsukapokem$bJdK7lgYDCQuP2SqOpvjBYcPvBQcUvAZzQKp6pjXXC5', '$5$rounds=5000$gabrielsukapokemon124qwe$', 1000, 0, 0, 0, 1000),
+(10, 'data6', '$5$rounds=5000$gabrielsukapokem$2FGZ/RY6fKw0NOU0bkTIEXbQeMucbhYEqDJANJ/8hD2', '$5$rounds=5000$gabrielsukapokemondata6$', 1000, 0, 0, 0, 1000),
+(11, 'data7', '$5$rounds=5000$gabrielsukapokem$Qofp2AdiHGgEIieNknYN9V6ECsRG..NKGDbLHsgXMI6', '$5$rounds=5000$gabrielsukapokemondata7$', 1000, 0, 0, 0, 1000),
+(13, 'data9', '$5$rounds=5000$gabrielsukapokem$XWQQiMq5mx4TAg7z362ndBUlf1pINOWN5N49hetj0C.', '$5$rounds=5000$gabrielsukapokemondata9$', 1000, 0, 0, 0, 1000),
+(14, 'data10', '$5$rounds=5000$gabrielsukapokem$4vg2ImldptwjbrFuofO3B6ZTuLYvC9mmoc5nECY/nM7', '$5$rounds=5000$gabrielsukapokemondata10$', 1000, 0, 0, 0, 1000);
 
 --
 -- Indexes for dumped tables
@@ -127,13 +123,6 @@ INSERT INTO `user` (`userID`, `username`, `hash`, `salt`, `score`, `win`, `lose`
 --
 ALTER TABLE `gamehistory`
   ADD PRIMARY KEY (`gameID`),
-  ADD KEY `userID` (`userID`);
-
---
--- Indexes for table `preference`
---
-ALTER TABLE `preference`
-  ADD PRIMARY KEY (`preferenceID`),
   ADD KEY `userID` (`userID`);
 
 --
@@ -158,25 +147,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `gamehistory`
 --
 ALTER TABLE `gamehistory`
-  MODIFY `gameID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `preference`
---
-ALTER TABLE `preference`
-  MODIFY `preferenceID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `gameID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `savedgames`
 --
 ALTER TABLE `savedgames`
-  MODIFY `savedGameID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `savedGameID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -187,12 +170,6 @@ ALTER TABLE `user`
 --
 ALTER TABLE `gamehistory`
   ADD CONSTRAINT `gamehistory_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `preference`
---
-ALTER TABLE `preference`
-  ADD CONSTRAINT `preference_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `savedgames`
